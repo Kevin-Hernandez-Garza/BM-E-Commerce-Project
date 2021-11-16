cbbeadf343e7626e667d7319085255cbb377fbe2
Marisol Bennett <marisol_ramirez21@live.com>
2021-11-15 20:03:07 -0600

    adding handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 const router = express.Router();
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
