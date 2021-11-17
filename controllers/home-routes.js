9cc028d02f5d84c3567e176531a341675b180a07
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-16 18:14:38 -0600

    added female products

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Female, Male, Review} = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
 
-    
+    Female.findAll({
+        attributes: [
+            'id',
+            'product_name',
+            'price',
+            'stock',
+            'photo',
+            'description'
+        ]
+    })
+        .then(dbFemaleData => {
+            const products = dbFemaleData.map(data => data.get({ plain: true }));
+            res.render('homepage', { products });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router; 
