0c24ab6f1137c27119161ee422f2ab5c7247bbac
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-16 23:08:49 -0600

    updating

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,27 +1,44 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { User, Female, Male, Review} = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User, Female, Male, Review } = require("../models");
 
-router.get('/', (req, res) => {
 
+router.get("/", (req, res) => {
     Female.findAll({
-        attributes: [
-            'id',
-            'product_name',
-            'price',
-            'stock',
-            'photo',
-            'description'
-        ]
+      attributes: [
+        "id",
+        "product_name",
+        "price",
+        "photo",
+      ],
     })
-        .then(dbFemaleData => {
-            const products = dbFemaleData.map(data => data.get({ plain: true }));
-            res.render('homepage', { products });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+      .then((dbFemaleData) => {
+        const products = dbFemaleData.map((data) => data.get({ plain: true }));
+        res.render("homepage", { products });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
-module.exports = router; 
+// router.get("/", (req, res) => {
+//     Male.findAll({
+//       attributes: [
+//         "id",
+//         "product_name",
+//         "price",
+//         "photo",
+//       ],
+//     })
+//       .then((dbMaleData) => {
+//         const maleProduct = dbMaleData.map((data) => data.get({ plain: true }));
+//         res.render("homepage", { maleProduct });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         res.status(500).json(err);
+//       });
+//   });
+
+module.exports = router;
